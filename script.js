ef50d65fd4e59d7fbc76a2bdc77123ba85a28ff4
kuldeep <kuldeepdhakad152@gmail.com>
2023-09-18 03:51:24 +0530

    some pages add

--- script.js
+++ script.js
@@ -122,3 +122,102 @@ var clutter=""
 		pin:true
 	}
   })
+  var swiper = new Swiper(".mySwiper", {
+	effect: "coverflow",
+	
+	coverflowEffect: {
+	  rotate: 0,
+	
+	  depth: 800,
+	//   modifier: 4,
+	//   slideShadows: true,
+	},
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+  });
+//   gsap.from("#page9 svg",{
+// 	// y:50000,
+// 	x:100,
+// 	// repeat:-1,
+// 	duration:2,
+// 	// delay:77,
+// 	scrollTrigger:{
+// 		scroller:"body",
+// 		trigger:"#page9",
+// 		scrub:2,
+// 		// start:"-250%",
+// 		// // end:"10%",
+// 		pin:true,
+// 		start:"0% 30%",
+// 		end:"0% 70%",
+// 		// markers:true
+// 	}
+
+//  })
+//  gsap.from("#page7 svg",{
+// 	// y:50000,
+// 	x:100,
+// 	// repeat:-1,
+// 	duration:2,
+// 	// delay:77,
+// 	scrollTrigger:{
+// 		scroller:"body",
+// 		trigger:"#page7",
+// 		scrub:2,
+// 		// start:"-250%",
+// 		// // end:"10%",
+// 		pin:true,
+// 		start:"0% 30%",
+// 		end:"0% 70%",
+// 		// markers:true
+// 	}
+
+//  })
+var clutter=""
+  var t1text=document.querySelector("#page8 h1").textContent
+  var splited=t1text.split("")
+  splited.forEach(function(elem){
+    clutter+=`<span>${elem}</span>`
+
+
+  })
+  document.querySelector("#page8 h1").innerHTML=clutter
+  gsap.from("#page8 h1 span",{
+	color:"#E3E3C4",
+	stagger:0.3,
+	scrollTrigger:{
+		scroller:"body",
+		trigger:"#page8",
+		scrub:1,
+		start:"top 10%",
+		end:"top -100%",
+		pin:true
+	}
+  })
+  var clutter=""
+  var t1text=document.querySelector("#page10 h1").textContent
+  var splited=t1text.split("")
+  splited.forEach(function(elem){
+    clutter+=`<span>${elem}</span>`
+
+
+  })
+  document.querySelector("#page10 h1").innerHTML=clutter
+  gsap.from("#page10 h1 span",{
+	color:"#434B34",
+	stagger:0.3,
+	scrollTrigger:{
+		scroller:"body",
+		trigger:"#page10",
+		scrub:1,
+		start:"top 10%",git   
+		end:"top -100%",
+		pin:true
+	}
+  })
